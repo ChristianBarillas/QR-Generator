58e60fc6426fd22ea9322b3b2e4bafec0387f8c4
christian barillas <70426461+ChristianBarillas@users.noreply.github.com>
2023-03-05 10:31:29 -0600

    adding js

---
+++ index.js
@@ -0,0 +1,36 @@
+const download = document.querySelector('.download');
+const sizes = document.querySelector('.sizes');
+const sharebtn = document.querySelector('.share-btn');
+const qrtext = document.querySelector('.qr-text');
+const qrcontainer = document.querySelector('#qr-code');
+const dark = document.querySelector('.dark');
+const light = document.querySelector('.light');
+
+
+dark.addEventListener('input', handleDarkColor);
+light.addEventListener('input', handleLightColor);
+qrtext.addEventListener('input', handleQRText);
+sizes.addEventListener('change', handleSize);
+sharebtn.addEventListener('click', handleShare);
+
+
+const defaultURL = "https://christianbarillas.github.io/Curriculum_Oficial/#"
+let colorLigth = '#fff',
+colorDark = '000',
+text = defaultURL,
+size = 300;
+
+function handleDarkColor(e){
+
+    colorDark = e.target.value;
+    generateQRcode;
+
+}
+
+
+function handleLightColor(e){
+
+    colorLigth = e.target.value;
+    generateQRcode;
+
+}
